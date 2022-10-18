0fbc60e9dac0b8344afc4be6ac7538daf8416891
mutiatsn <mtsaniaz@gmail.com>
2022-10-18 16:21:46 +0700

    Latihan_Aplikasi_Android_1

---
+++ app/src/main/java/com/example/latihan1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.latihan1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.latihan1.ui.theme.Latihan1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Latihan1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Mutia Tsani Az Zahra")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Latihan1Theme {
+        Greeting("Mutia Tsani Az Zahra")
+    }
+}
