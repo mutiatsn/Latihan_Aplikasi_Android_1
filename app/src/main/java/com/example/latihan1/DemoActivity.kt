f88867de9878cd8830a9f65de8c640b63f1ecf45
mutiatsn <mtsaniaz@gmail.com>
2022-10-25 16:40:10 +0700

    ProgressDemoActivity

--- app/src/main/java/com/example/latihan1/DemoActivity.kt
+++ app/src/main/java/com/example/latihan1/DemoActivity.kt
@@ -3,13 +3,19 @@ package com.example.latihan1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -17,16 +23,27 @@ class DemoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)
+            )
+            {
+                Image(
+                    painter = painterResource(R.drawable.gdsate),
+                    contentDescription = "Gambar")
                 Surface(
                     color = Color.Yellow,
                     modifier = Modifier.fillMaxWidth()
+
                 ) {
                     Text(
                         text = "Hallo hallo Bandung",
                         fontSize = 24.sp,
                         modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 4.dp),
-                        color = Color.Magenta
+                        color = Color.Magenta,
+                        textAlign = TextAlign.Center
                     )
                 }
 
@@ -38,7 +55,8 @@ class DemoActivity : ComponentActivity() {
                         text = "Ibu Kota Perjuangan",
                         fontSize = 20.sp,
                         modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 4.dp),
-                        color = Color.Blue
+                        color = Color.Blue,
+                        textAlign = TextAlign.Center
                     )
                 }
             }
